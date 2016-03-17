c17ef42e8271f7259f6af566364e33f41c4dc04e
as3388627 <858687005@qq.com>
2016-03-17 17:34:04 +0800

    写了一个通用service

--- src/com/ffcs/sys/service/SysGroupService.java
+++ src/com/ffcs/sys/service/SysGroupService.java
@@ -2,16 +2,6 @@ package com.ffcs.sys.service;
 
 import com.ffcs.sys.entity.SysGroup;
 
-public interface SysGroupService {
-    int deleteByPrimaryKey(Integer groupId);
-
-    int insert(SysGroup record);
-
-    int insertSelective(SysGroup record);
-
-    SysGroup selectByPrimaryKey(Integer groupId);
-
-    int updateByPrimaryKeySelective(SysGroup record);
-
-    int updateByPrimaryKey(SysGroup record);
+public interface SysGroupService extends CommService<SysGroup> {
+    
 }
