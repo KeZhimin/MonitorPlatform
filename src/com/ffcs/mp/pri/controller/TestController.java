a4d30bd81074cbc0d9db1bf402452200db62593b
KeZhimin <kezhimin130121@gmail.com>
2016-03-17 09:31:33 +0800

    搭建

---
+++ src/com/ffcs/mp/pri/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.ffcs.mp.pri.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ffcs.mp.entity.User;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("test")
+	public String test() {
+		System.out.println("hahahhah");
+		return "test/index";
+	}
+
+	@ResponseBody
+	@RequestMapping("testJson")
+	public String testJson() {
+		System.out.println("this is testJson...");
+		return "0";
+	}
+
+	@ResponseBody
+	@RequestMapping(value="getUser")
+	public User getUser(){
+		System.out.println("this is getUser...");
+		User user = new User();
+		user.setId("1");
+		user.setName("¿ÂÖÇÃô");
+		return user;
+	}
+	
+	
+}
